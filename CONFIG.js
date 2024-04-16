b95005245a68f940b7c575a87a2c334fd6e9a669
Shleldq <167206686+Shleldq@users.noreply.github.com>
2024-04-16 20:06:19 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào Huyền, A có điều muốn nói. </br> Hãy cho anh quay lại theo đuối em nhé!",
+  titleHeader: "Chào Huyền, Anh có điều muốn nói. </br> Hãy cho anh quay lại theo đuổi em nhé!",
   descriptionHeader:
     "Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
   buttonYes: "Em đồng ý",
   buttonNo: "Không đồng ý",
   titleModar: "Cảm ơn em.",
   descriptionModar:
-    "Cảm ơn em đã cho anh một cơ hội để có thể ở bên cạnh, che chở và bảo vệ em. </br> Anh biết anh đã làm em thất vọng, cảm ơn em đã cho anh cơ hội để sửa sai. </br> Anh hứa sẽ không có lần 2 như vậy nữa!.</br> Love you <3",
+    "Cảm ơn em đã cho anh một cơ hội để có thể quay lại với em. </br> Anh biết anh đã làm em thất vọng, cảm ơn em đã cho anh cơ hội để sửa sai. </br> Anh hứa sẽ không có lần 2 như vậy nữa!.</br> Love you <3",
 };
